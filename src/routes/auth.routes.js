718da56e3f42c4129cbfacad805778ff2d14a8d0
Anjali Dubey <anjalidubey021@gmail.com>
2026-06-16 21:36:53 +0530

    added /register api

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const userModel = require('../models/user.model');
 
 const router = express.Router();
 
@@ -7,5 +8,17 @@ const router = express.Router();
  * POST /login
  * get /user [protected]
  */
+
+router.post('/register', async(req,res)=>{
+    const {username, password} = req.body;
+
+    const user = await userModel.create({username, password}); 
+    res.status(201).json({
+        success: true,
+        message: "User registered successfully",
+        user
+    });
+});
+
 
 module.exports = router;

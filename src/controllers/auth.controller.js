c276ccd547dc1e1331ad827570dedeb874ef0125
Anjali Dubey <anjalidubey021@gmail.com>
2026-06-18 20:11:08 +0530

    added login controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -26,6 +26,37 @@ async function register(req, res) {
       
 }
 
+async function login(req, res) {
+    const {username, password} = req.body;
+
+    const user = await userModel.findOne({username});
+
+    if(!user){
+        return res.status(400).json({
+            success: false,
+            message: "User not found"
+        });
+    }
+
+    const isPasswordValid = user.password === password;
+    if(!isPasswordValid){
+        return res.status(400).json({
+            success: false,
+            message: "Invalid password"
+        });
+    }
+
+    const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET);
+    res.cookie('token', token);
+
+    res.status(200).json({
+        success: true,
+        message: "User logged in successfully",
+        user
+    });
+}
+
 module.exports = {
-    register
+    register,
+    login
 };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -10,5 +10,6 @@ const authController = require('../controllers/auth.controller');
 
 router.post('/register', authController.register);
 
+router.post('/login', authController.login);
 
 module.exports = router;

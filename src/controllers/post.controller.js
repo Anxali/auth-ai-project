d51e9e560ddce77d9f06bccedc6a827a27b6b41d
Anjali Dubey <anjalidubey021@gmail.com>
2026-06-25 22:04:13 +0530

    added console log for base 64 img

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -2,7 +2,11 @@ const postModel = require('../models/post.model');
 
 async function createPostController(req,res){
     const file = req.file;
-    console.log("file received in controller: ", file);   
+    //console.log("file received in controller: ", file);   
+
+    const base64ImageFile = Buffer.from(file.buffer).toString('base64');
+
+    console.log("base64ImageFile: ", base64ImageFile);
 }
 
 module.exports = {
